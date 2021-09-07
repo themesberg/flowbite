68d1054f666ee485d7dd8f0d6bf8de9539e1d272
Zoltán Szőgyényi <zoltan.szogyenyi@gmail.com>
2021-09-07 13:13:04 +0300

    Fix scroll issue from the left sidebar.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   purge: {
-    enabled: true,
+    // enabled: true,
     content: ["./layouts/**/*.html", "./content/**/*.md", "./content/**/*.html"],
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
     fontFamily: {
-      'sans': ['Inter', 'sans-serif'],
-      'body': ['Inter', 'sans-serif'],
+      'sans': ['Inter', 'ui-sans-serif', 'system-ui', '-apple-system', 'system-ui', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
+      'body': ['Inter', 'ui-sans-serif', 'system-ui', '-apple-system', 'system-ui', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
       'mono': ['ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', 'Liberation Mono', 'Courier New', 'monospace']
     },
     colors: {

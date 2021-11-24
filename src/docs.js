75e6446d3bddc573cc75eadf705e6773806d6c88
Zoltán Szőgyényi <zoltan.szogyenyi@gmail.com>
2021-11-24 16:15:59 +0200

    Theme color toggle.

--- src/docs.js
+++ src/docs.js
@@ -63,18 +63,36 @@ document.addEventListener("DOMContentLoaded", function () {
 
 });
 
+const themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+const themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+
 // On page load or when changing themes, best to add inline in `head` to avoid FOUC
-if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    document.documentElement.classList.add('dark')
+if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.documentElement.classList.add('dark');
+    
+    // toggle icons
+    themeToggleDarkIcon.classList.toggle('hidden');
+    themeToggleLightIcon.classList.toggle('hidden');
 } else {
     document.documentElement.classList.remove('dark')
 }
 
-// Whenever the user explicitly chooses light mode
-// localStorage.theme = 'light'
+const themeToggleBtn = document.getElementById('theme-toggle');
 
-// Whenever the user explicitly chooses dark mode
-// localStorage.theme = 'dark'
+themeToggleBtn.addEventListener('click', () => {
+
+    // toggle icons
+    themeToggleDarkIcon.classList.toggle('hidden');
+    themeToggleLightIcon.classList.toggle('hidden');
+
+    if (localStorage.getItem('color-theme') === 'light') {
+        document.documentElement.classList.add('dark');
+        localStorage.setItem('color-theme', 'dark');
+    } else {
+        document.documentElement.classList.remove('dark');
+        localStorage.setItem('color-theme', 'light');
+    }
+});
 
 // Whenever the user explicitly chooses to respect the OS preference
 // localStorage.removeItem('theme')

17acee7638dfdd1b4f734faf63096ab6351aa1f9
Tanislav Robert <tanislav.robert@gmail.com>
2021-11-24 16:21:36 +0200

    Improve dark/light mode checking when not set via localStorage.

--- src/docs.js
+++ src/docs.js
@@ -85,13 +85,27 @@ themeToggleBtn.addEventListener('click', () => {
     themeToggleDarkIcon.classList.toggle('hidden');
     themeToggleLightIcon.classList.toggle('hidden');
 
-    if (localStorage.getItem('color-theme') === 'light') {
-        document.documentElement.classList.add('dark');
-        localStorage.setItem('color-theme', 'dark');
+    // if set via local storage previously
+    if (localStorage.getItem('color-theme')) {
+        if (localStorage.getItem('color-theme') === 'light') {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('color-theme', 'dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('color-theme', 'light');
+        }
+
+    // if NOT set via local storage previously
     } else {
-        document.documentElement.classList.remove('dark');
-        localStorage.setItem('color-theme', 'light');
+        if (document.documentElement.classList.contains('dark')) {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('color-theme', 'light');
+        } else {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('color-theme', 'dark');
+        }
     }
+    
 });
 
 // Whenever the user explicitly chooses to respect the OS preference

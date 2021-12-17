3226acbbb544a327722ef89e7b6f9651fed15ed4
Zoltán Szőgyényi <zoltan.szogyenyi@gmail.com>
2021-12-17 16:10:46 +0200

    Improve sidebar functionality on mobile.

--- src/docs.js
+++ src/docs.js
@@ -106,3 +106,25 @@ themeToggleBtn.addEventListener('click', function() {
     }
     
 });
+
+const sidebar = document.getElementById('sidebar');
+
+const toggleSidebarMobile = (sidebar, sidebarBackdrop, toggleSidebarMobileHamburger, toggleSidebarMobileClose) => {
+    sidebar.classList.toggle('hidden');
+    sidebarBackdrop.classList.toggle('hidden');
+    toggleSidebarMobileHamburger.classList.toggle('hidden');
+    toggleSidebarMobileClose.classList.toggle('hidden');
+}
+
+const toggleSidebarMobileEl = document.getElementById('toggleSidebarMobile');
+const sidebarBackdrop = document.getElementById('sidebarBackdrop');
+const toggleSidebarMobileHamburger = document.getElementById('toggleSidebarMobileHamburger');
+const toggleSidebarMobileClose = document.getElementById('toggleSidebarMobileClose');
+
+toggleSidebarMobileEl.addEventListener('click', () => {
+    toggleSidebarMobile(sidebar, sidebarBackdrop, toggleSidebarMobileHamburger, toggleSidebarMobileClose);
+});
+
+sidebarBackdrop.addEventListener('click', () => {
+    toggleSidebarMobile(sidebar, sidebarBackdrop, toggleSidebarMobileHamburger, toggleSidebarMobileClose);
+});

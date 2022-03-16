b7693a9104364772f31a382dfe74376a07c8ece7
Zoltán Szőgyényi <zoltan.szogyenyi@gmail.com>
2022-03-16 14:42:05 +0200

    Refactor Accordion JS.

--- content/components/accordion.md
+++ content/components/accordion.md
@@ -30,12 +30,12 @@ Use the `data-accordion="collapse"` to collapse every other child element when e
 {{< example github="components/accordion.md" show_dark=true >}}
 <div id="accordion-collapse" data-accordion="collapse">
   <h2 id="accordion-collapse-heading-1">
-    <button type="button" class="flex justify-between items-center p-5 w-full font-medium text-left text-gray-900 bg-gray-100 rounded-t-xl border border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-white dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-800" data-accordion-target="#accordion-collapse-body-1" aria-expanded="true" aria-controls="accordion-collapse-body-1">
+    <button type="button" class="flex justify-between items-center p-5 w-full font-medium text-left text-gray-500 rounded-t-xl border border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800" data-accordion-target="#accordion-collapse-body-1" aria-expanded="true" aria-controls="accordion-collapse-body-1">
       <span>What is Flowbite?</span>
       <svg data-accordion-icon class="w-6 h-6 rotate-180 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
     </button>
   </h2>
-  <div id="accordion-collapse-body-1" aria-labelledby="accordion-collapse-heading-1">
+  <div id="accordion-collapse-body-1" class="hidden" aria-labelledby="accordion-collapse-heading-1">
     <div class="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
       <p class="mb-2 text-gray-500 dark:text-gray-400">Flowbite is an open-source library of interactive components built on top of Tailwind CSS including buttons, dropdowns, modals, navbars, and more.</p>
       <p class="text-gray-500 dark:text-gray-400">Check out this guide to learn how to <a href="https://flowbite.com/docs/getting-started/introduction/" class="text-blue-600 dark:text-blue-500 hover:underline">get started</a> and start developing websites even faster with components on top of Tailwind CSS.</p>
@@ -80,12 +80,12 @@ Use the `data-accordion="open"` option to keep previously opened accordion eleme
 {{< example github="components/accordion.md" show_dark=true >}}
 <div id="accordion-open" data-accordion="open">
   <h2 id="accordion-open-heading-1">
-    <button type="button" class="flex justify-between items-center p-5 w-full font-medium text-left text-gray-900 bg-gray-100 rounded-t-xl border border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-white dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-800" data-accordion-target="#accordion-open-body-1" aria-expanded="true" aria-controls="accordion-open-body-1">
+    <button type="button" class="flex justify-between items-center p-5 w-full font-medium text-left text-gray-500 rounded-t-xl border border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800" data-accordion-target="#accordion-open-body-1" aria-expanded="true" aria-controls="accordion-open-body-1">
       <span class="flex items-center"><svg class="mr-2 w-5 h-5 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg> What is Flowbite?</span>
       <svg data-accordion-icon class="w-6 h-6 rotate-180 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
     </button>
   </h2>
-  <div id="accordion-open-body-1" aria-labelledby="accordion-open-heading-1">
+  <div id="accordion-open-body-1" class="hidden" aria-labelledby="accordion-open-heading-1">
     <div class="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
       <p class="mb-2 text-gray-500 dark:text-gray-400">Flowbite is an open-source library of interactive components built on top of Tailwind CSS including buttons, dropdowns, modals, navbars, and more.</p>
       <p class="text-gray-500 dark:text-gray-400">Check out this guide to learn how to <a href="https://flowbite.com/docs/getting-started/introduction/" class="text-blue-600 dark:text-blue-500 hover:underline">get started</a> and start developing websites even faster with components on top of Tailwind CSS.</p>
@@ -132,12 +132,12 @@ Here's an example where we apply the blue colors instead of gray:
 {{< example github="components/accordion.md" show_dark=true >}}
 <div id="accordion-color" data-accordion="collapse" data-active-classes="bg-blue-100 dark:bg-gray-800 text-blue-600 dark:text-white">
   <h2 id="accordion-color-heading-1">
-    <button type="button" class="flex justify-between items-center p-5 w-full font-medium text-left text-blue-600 bg-blue-100 rounded-t-xl border border-b-0 border-gray-200 focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-800 dark:border-gray-700 dark:text-white dark:bg-gray-800 hover:bg-blue-100 dark:hover:bg-gray-800" data-accordion-target="#accordion-color-body-1" aria-expanded="true" aria-controls="accordion-color-body-1">
+    <button type="button" class="flex justify-between items-center p-5 w-full font-medium text-left text-gray-500 rounded-t-xl border border-b-0 border-gray-200 focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-800 dark:border-gray-700 dark:text-gray-400 hover:bg-blue-100 dark:hover:bg-gray-800" data-accordion-target="#accordion-color-body-1" aria-expanded="true" aria-controls="accordion-color-body-1">
       <span>What is Flowbite?</span>
       <svg data-accordion-icon class="w-6 h-6 rotate-180 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
     </button>
   </h2>
-  <div id="accordion-color-body-1" aria-labelledby="accordion-color-heading-1">
+  <div id="accordion-color-body-1" class="hidden" aria-labelledby="accordion-color-heading-1">
     <div class="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
       <p class="mb-2 text-gray-500 dark:text-gray-400">Flowbite is an open-source library of interactive components built on top of Tailwind CSS including buttons, dropdowns, modals, navbars, and more.</p>
       <p class="text-gray-500 dark:text-gray-400">Check out this guide to learn how to <a href="https://flowbite.com/docs/getting-started/introduction/" class="text-blue-600 dark:text-blue-500 hover:underline">get started</a> and start developing websites even faster with components on top of Tailwind CSS.</p>
@@ -182,12 +182,12 @@ Use this example to remove the background color and rounded borders from the acc
 {{< example github="components/accordion.md" show_dark=true >}}
 <div id="accordion-flush" data-accordion="collapse" data-active-classes="bg-white dark:bg-gray-900 text-gray-900 dark:text-white" data-inactive-classes="text-gray-500 dark:text-gray-400">
   <h2 id="accordion-flush-heading-1">
-    <button type="button" class="flex justify-between items-center py-5 w-full font-medium text-left text-gray-900 rounded-t-xl border-b border-gray-200 dark:border-gray-700 dark:text-white" data-accordion-target="#accordion-flush-body-1" aria-expanded="true" aria-controls="accordion-flush-body-1">
+    <button type="button" class="flex justify-between items-center py-5 w-full font-medium text-left text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400" data-accordion-target="#accordion-flush-body-1" aria-expanded="true" aria-controls="accordion-flush-body-1">
       <span>What is Flowbite?</span>
       <svg data-accordion-icon class="w-6 h-6 rotate-180 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
     </button>
   </h2>
-  <div id="accordion-flush-body-1" aria-labelledby="accordion-flush-heading-1">
+  <div id="accordion-flush-body-1" class="hidden" aria-labelledby="accordion-flush-heading-1">
     <div class="py-5 border-b border-gray-200 dark:border-gray-700">
       <p class="mb-2 text-gray-500 dark:text-gray-400">Flowbite is an open-source library of interactive components built on top of Tailwind CSS including buttons, dropdowns, modals, navbars, and more.</p>
       <p class="text-gray-500 dark:text-gray-400">Check out this guide to learn how to <a href="https://flowbite.com/docs/getting-started/introduction/" class="text-blue-600 dark:text-blue-500 hover:underline">get started</a> and start developing websites even faster with components on top of Tailwind CSS.</p>
--- src/components/accordion.js
+++ src/components/accordion.js
@@ -1,205 +1,144 @@
-// const hideAllOtherAccordionHeaderElements = (accordionHeaderElements, currentAccordionHeaderEl, activeClassesArray, inactiveClassesArray) => {
-//     accordionHeaderElements.forEach(headerEl => {
-//         if (currentAccordionHeaderEl !== headerEl) {
-//             const bodyEl = document.querySelector(headerEl.getAttribute('data-accordion-target'));
-//             headerEl.setAttribute('aria-expanded', false);
-//             // active classes
-//             activeClassesArray.map(c => {
-//                 headerEl.classList.remove(c);
-//             })
-//             // inactive classes
-//             inactiveClassesArray.map(c => {
-//                 headerEl.classList.add(c);
-//             })
-//             bodyEl.classList.add('hidden');
-    
-//             if (headerEl.querySelector('[data-accordion-icon]')) {
-//                 headerEl.querySelector('[data-accordion-icon]').classList.remove('rotate-180');
-//             }
-//         }
-//     });
-// }
-
-// const rotateAccordionIcon = (accordionHeaderEl) => {
-//     if (accordionHeaderEl.querySelector('[data-accordion-icon]')) {
-//         accordionHeaderEl.querySelector('[data-accordion-icon]').classList.toggle('rotate-180');
-//     }
-// }
-
-// document.addEventListener('DOMContentLoaded', () => {
-//     document.querySelectorAll('[data-accordion]').forEach(function (accordionEl) {
-//         const accordionId = accordionEl.getAttribute('id');
-//         const collapseAccordion = accordionEl.getAttribute('data-accordion');
-//         const accordionHeaderElements = document.querySelectorAll('#' + accordionId + ' [data-accordion-target]');
-//         const activeClasses = accordionEl.getAttribute('data-active-classes');
-//         const inactiveClasses = accordionEl.getAttribute('data-inactive-classes');
-        
-//         let activeClassesArray = null;
-//         if (activeClasses && activeClasses !== '') {
-//             activeClassesArray = activeClasses.split(" ");
-//         } else {
-//             // fallback classes if option not set
-//             activeClassesArray = ['bg-gray-100', 'dark:bg-gray-800', 'text-gray-900', 'dark:text-white'];
-//         }
-
-//         let inactiveClassesArray = null;
-//         if (inactiveClasses && inactiveClasses !== '') {
-//             inactiveClassesArray = inactiveClasses.split(" ");
-//         } else {
-//             // fallback classes if option not set
-//             inactiveClassesArray = ['text-gray-500', 'dark:text-gray-400'];
-//         }
-
-//         accordionHeaderElements.forEach(accordionHeaderEl => {
-
-//             const accordionBodyEl = document.querySelector(accordionHeaderEl.getAttribute('data-accordion-target'));
-
-//             accordionHeaderEl.addEventListener('click', () => {
-
-//                 if (collapseAccordion === 'collapse') {
-//                     hideAllOtherAccordionHeaderElements(accordionHeaderElements, accordionHeaderEl, activeClassesArray, inactiveClassesArray);
-//                 }
-
-//                 if (accordionHeaderEl.getAttribute('aria-expanded') === 'true') {
-//                     accordionHeaderEl.setAttribute('aria-expanded', false);
-
-//                     // active classes
-//                     activeClassesArray.map(c => {
-//                         accordionHeaderEl.classList.remove(c);
-//                     })
-
-//                     // inactive classes
-//                     inactiveClassesArray.map(c => {
-//                         accordionHeaderEl.classList.add(c);
-//                     })
-
-//                     accordionBodyEl.classList.add('hidden');
-
-//                     rotateAccordionIcon(accordionHeaderEl);
-//                 } else {
-//                     accordionHeaderEl.setAttribute('aria-expanded', true);
-
-//                     // active classes
-//                     activeClassesArray.map(c => {
-//                         accordionHeaderEl.classList.add(c);
-//                     })
-
-//                     // inactive classes
-//                     inactiveClassesArray.map(c => {
-//                         accordionHeaderEl.classList.remove(c);
-//                     })
-
-//                     accordionBodyEl.classList.remove('hidden');
-
-//                     rotateAccordionIcon(accordionHeaderEl);
-//                 }
-//             })
-
-//         });
-//     });
-// });
-
 const Default = {
-    onShow: () => { }
+    alwaysOpen: false,
+    activeClasses: ['bg-gray-100', 'dark:bg-gray-800', 'text-gray-900', 'dark:text-white'],
+    inactiveClasses: ['text-gray-500', 'dark:text-gray-400'],
+    onShow: () => { },
+    onHide: () => { }
 }
 
 class Accordion {
-    constructor(tabs = [], activeTabID = null, options = {}) {
-        this._tabs = tabs
-        this._activeTab = activeTabID ? this.getTabByID(activeTabID) : null
+    constructor(items = [], options = {}) {
+        this._items = items
         this._options = { ...Default, ...options }
         this._init()
     }
 
     _init() {
-        if (this._tabs.length) {
-            // set the first tab as active if not set by explicitly
-            if (!this._activeTab) {
-                this._setActiveTab(this._tabs[0])
-            }
+        if (this._items.length) {
+            // show accordion item based on click
+            this._items.map(item => {
 
-            // force show the first default tab
-            this.show(this._activeTab.id, true)
+                if (item.active) {
+                    this.show(item.id)
+                }
 
-            // show tab content based on click
-            this._tabs.map(tab => {
-                tab.triggerEl.addEventListener('click', () => {
-                    this.show(tab.id)
+                item.triggerEl.addEventListener('click', () => {
+                    this.toggle(item.id)
                 })
             })
         }
     }
 
-    getActiveTab() {
-        return this._activeTab
+    getItem(id) {
+        return this._items.filter(item => item.id === id)[0]
     }
 
-    _setActiveTab(tab) {
-        this._activeTab = tab
-    }
+    show(id) {
+        const item = this.getItem(id)
+        console.log('ITEM')
+        console.log(item)
 
-    getTabByID(id) {
-        return this._tabs.filter(t => t.id === id)[0]
-    }
+        // don't hide other accordions if always open
+        if (!this._options.alwaysOpen) {
+            this._items.map(i => {
+                if (i !== item) {
+                    i.triggerEl.classList.remove(...this._options.activeClasses)
+                    i.triggerEl.classList.add(...this._options.inactiveClasses)
+                    i.contentEl.classList.add('hidden')
+                    i.triggerEl.setAttribute('aria-expanded', false)
+                    i.active = false
 
-    show(id, forceShow = false) {
-        const tab = this.getTabByID(id)
-
-        // don't do anything if already active
-        if (tab === this._activeTab && !forceShow) {
-            return
+                    // rotate icon if set
+                    if (i.iconEl) {
+                        i.iconEl.classList.remove('rotate-180')
+                    }
+                }
+            })
         }
 
-        // hide other tabs
-        this._tabs.map(t => {
-            if (t !== tab) {
-                t.triggerEl.classList.remove('active')
-                t.contentEl.classList.add('hidden')
-                t.triggerEl.setAttribute('aria-selected', false)
-            }
-        })
 
-        // show active tab
-        tab.triggerEl.classList.add('active')
-        tab.triggerEl.setAttribute('aria-selected', true)
-        tab.contentEl.classList.remove('hidden')
+        // show active item
+        item.triggerEl.classList.add(...this._options.activeClasses)
+        item.triggerEl.classList.remove(...this._options.inactiveClasses)
+        item.triggerEl.setAttribute('aria-expanded', true)
+        item.contentEl.classList.remove('hidden')
+        item.active = true
 
-        this._setActiveTab(tab)
+        // rotate icon if set
+        if (item.iconEl) {
+            item.iconEl.classList.add('rotate-180')
+        }
 
         // callback function
         this._options.onShow()
+    }
+
+    toggle(id) {
+        const item = this.getItem(id)
+
+        if (item.active) {
+            this.hide(id)
+        } else {
+            this.show(id)
+        }
+    }
+
+    hide(id) {
+        const item = this.getItem(id)
+        console.log('ITEM')
+        console.log(item)
+
+        item.triggerEl.classList.remove(...this._options.activeClasses)
+        item.triggerEl.classList.add(...this._options.inactiveClasses)
+        item.contentEl.classList.add('hidden')
+        item.triggerEl.setAttribute('aria-expanded', false)
+        item.active = false
+
+        // rotate icon if set
+        if (item.iconEl) {
+            item.iconEl.classList.remove('rotate-180')
+        }
+
+        // callback function
+        this._options.onHide()
     }
 
 }
 
 window.Accordion = Accordion;
 
-// document.addEventListener('DOMContentLoaded', () => {
-//     document.querySelectorAll('[data-tabs-toggle]').forEach(triggerEl => {
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('[data-accordion]').forEach(accordionEl => {
 
-//         const tabElements = []
-//         let activeTabID = null
-//         triggerEl.querySelectorAll('[role="tab"]').forEach(el => {
-//             const isActive = el.getAttribute('aria-selected') === 'true'
-//             const tab = {
-//                 id: el.getAttribute('data-tabs-target'),
-//                 triggerEl: el,
-//                 contentEl: document.querySelector(el.getAttribute('data-tabs-target'))
-//             }
-//             tabElements.push(tab)
+        const alwaysOpen = accordionEl.getAttribute('data-accordion')
+        const activeClasses = accordionEl.getAttribute('data-active-classes') ? accordionEl.getAttribute('data-active-classes').split(" ") : []
+        const inactiveClasses = accordionEl.getAttribute('data-inactive-classes') ? accordionEl.getAttribute('data-inactive-classes').split(" ") : []
 
-//             if (isActive) {
-//                 activeTabID = tab.id
-//             }
-//         })
+        const items = []
+        accordionEl.querySelectorAll('[data-accordion-target]').forEach(el => {
+            const item = {
+                id: el.getAttribute('data-accordion-target'),
+                triggerEl: el,
+                contentEl: document.querySelector(el.getAttribute('data-accordion-target')),
+                iconEl: el.querySelector('[data-accordion-icon]'),
+                active: el.getAttribute('aria-expanded') === 'true' ? true : false
+            }
+            items.push(item)
+        })
 
-//         const tabs = new Tabs(tabElements, activeTabID, {
-//             onShow: () => {
-//                 console.log('tab is shown')
-//             }
-//         })
-//         console.log(tabs)
-//     })
-// })
+        const accordion = new Accordion(items, {
+            alwaysOpen: alwaysOpen === 'open' ? true : false,
+            activeClasses: activeClasses.length ? activeClasses : [...Default.activeClasses],
+            inactiveClasses: inactiveClasses.length ? inactiveClasses : [...Default.inactiveClasses],
+            onShow: () => {
+                console.log('accordion item has been shown')
+            },
+            onHide: () => {
+                console.log('accordion item has been hidden')
+            }
+        })
+        console.log(accordion)
+    })
+})
 
 export default Accordion

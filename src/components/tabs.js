02c8f642121a68a2a56bc432efaa75dc8cbc506d
Zoltán Szőgyényi <zoltan.szogyenyi@gmail.com>
2022-05-04 12:51:56 +0300

    Update default active classes for tabs.

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,7 +1,7 @@
 const Default = {
     defaultTabId: null,
-    activeClasses: 'text-blue-600 hover:text-blue-600 dark:text-blue-500 dark:hover:text-blue-400 border-blue-600 dark:border-blue-500',
-    inactiveClasses: 'text-gray-500 hover:text-gray-600 dark:text-gray-400 border-gray-100 hover:border-gray-300 dark:border-gray-700 dark:hover:text-gray-300',
+    activeClasses: 'text-blue-600 hover:text-blue-600 dark:text-blue-500 dark:hover:text-blue-500 border-blue-600 dark:border-blue-500',
+    inactiveClasses: 'dark:border-transparent text-gray-500 hover:text-gray-600 dark:text-gray-400 border-gray-100 hover:border-gray-300 dark:border-gray-700 dark:hover:text-gray-300',
     onShow: () => { }
 }
 

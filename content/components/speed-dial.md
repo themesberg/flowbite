b0d6a9d92983726fbf1d5b8669ef0a787dcc239b
Zoltán Szőgyényi <zoltan.szogyenyi@gmail.com>
2022-11-08 15:19:53 +0200

    Add click + hover trigger type options.

--- content/components/speed-dial.md
+++ content/components/speed-dial.md
@@ -16,7 +16,7 @@ nextLink: components/tables/
 ## Default speed dial
 
 {{< example id="default-speed-dial-example" class="flex justify-center pt-24 h-96" github="components/speed-dial.md" show_dark=true >}}
-<div class="fixed right-8 bottom-8">
+<div data-dial-init class="fixed right-8 bottom-8 group">
     <div id="speed-dial-menu-default" class="flex hidden flex-col items-center mb-4 space-y-2">
         <button type="button" data-tooltip-target="tooltip-share" data-tooltip-placement="left" class="flex justify-center items-center w-[52px] h-[52px] text-gray-500 bg-white rounded-lg border border-gray-200 dark:border-gray-600 shadow-sm dark:hover:text-white dark:text-gray-400 hover:bg-gray-50 dark:bg-gray-700 dark:hover:bg-gray-600 focus:ring-4 focus:ring-gray-300 focus:outline-none dark:focus:ring-gray-400">
             <svg aria-hidden="true" class="-ml-px w-6 h-6 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M15 8a3 3 0 10-2.977-2.63l-4.94 2.47a3 3 0 100 4.319l4.94 2.47a3 3 0 10.895-1.789l-4.94-2.47a3.027 3.027 0 000-.74l4.94-2.47C13.456 7.68 14.19 8 15 8z"></path></svg>
@@ -51,8 +51,8 @@ nextLink: components/tables/
             <div class="tooltip-arrow" data-popper-arrow></div>
         </div>
     </div>
-    <button type="button" data-collapse-toggle="speed-dial-menu-default" aria-controls="speed-dial-menu-default" aria-expanded="false" class="flex justify-center items-center w-14 h-14 text-white bg-blue-700 rounded-lg group hover:bg-blue-800 dark:bg-blue-600 dark:hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:focus:ring-blue-800">
-        <svg aria-hidden="true" class="w-8 h-8 rotate-45" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg>
+    <button type="button" data-dial-toggle="speed-dial-menu-default" aria-controls="speed-dial-menu-default" aria-expanded="false" class="flex justify-center items-center w-14 h-14 text-white bg-blue-700 rounded-lg hover:bg-blue-800 dark:bg-blue-600 dark:hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:focus:ring-blue-800">
+        <svg aria-hidden="true" class="w-8 h-8 transition-transform group-hover:rotate-45" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg>
         <span class="sr-only">Open actions menu</span>
     </button>
 </div>
---
+++ src/components/dial.js
@@ -0,0 +1,128 @@
+const Default = {
+    targetEl: null,
+    triggerEl: null,
+    triggerType: 'hover',
+    onShow: () => { },
+    onHide: () => { },
+    onToggle: () => { }
+}
+
+class Dial {
+    constructor(parentEl = null, options) {
+        this._parentEl = parentEl
+        this._triggerEl = options ? options.triggerEl : Default.triggerEl
+        this._targetEl = options ? options.targetEl : Default.targetEl
+        this._options = { ...Default, ...options }
+        this._visible = false
+        this._init()
+    }
+
+    _init() {
+
+        console.log(this)
+
+        if (this._triggerEl) {
+            const triggerEvents = this._getTriggerEvents()
+            triggerEvents.showEvents.forEach(ev => {
+                this._triggerEl.addEventListener(ev, () => {
+                    this.show()
+                })
+                this._targetEl.addEventListener(ev, () => {
+                    this.show()
+                })
+            })
+            triggerEvents.hideEvents.forEach(ev => {
+                this._triggerEl.addEventListener(ev, () => {
+                    setTimeout(() => {
+                        if (!this._targetEl.matches(':hover')) {
+                            this.hide()
+                        }
+                    }, 100)
+                })
+                this._targetEl.addEventListener(ev, () => {
+                    setTimeout(() => {
+                        if (!this._triggerEl.matches(':hover')) {
+                            this.hide()
+                        }
+                    }, 100)
+                })
+            })
+        }
+
+    }
+
+    hide() {
+        this._targetEl.classList.add('hidden')
+        if(this._triggerEl) {
+            this._triggerEl.setAttribute('aria-expanded', 'false')
+        }
+        this._visible = false
+
+        // callback function
+        this._options.onHide(this)
+    }
+
+    show() {
+        this._targetEl.classList.remove('hidden')
+        if(this._triggerEl) {
+            this._triggerEl.setAttribute('aria-expanded', 'true')
+        }
+        this._visible = true
+
+        // callback function
+        this._options.onShow(this)
+    }
+
+    _getTriggerEvents() {
+        switch (this._options.triggerType) {
+            case 'hover':
+                return {
+                    showEvents: ['mouseenter', 'focus'],
+                    hideEvents: ['mouseleave', 'blur']
+                }
+            case 'click':
+                return {
+                    showEvents: ['click', 'focus'],
+                    hideEvents: ['focusout', 'blur']
+                }
+            default:
+                return {
+                    showEvents: ['mouseenter', 'focus'],
+                    hideEvents: ['mouseleave', 'blur']
+                }
+        }
+    }
+
+    toggle() {
+        if (this._visible) {
+            this.hide()
+        } else {
+            this.show()
+        }
+    }
+
+}
+
+window.Dial = Dial;
+
+function initDial() {
+    document.querySelectorAll('[data-dial-init]').forEach(parentEl => {
+        const triggerEl = parentEl.querySelector('[data-dial-toggle]')
+        const targetEl = document.getElementById(triggerEl.getAttribute('data-dial-toggle'))
+
+        new Dial(parentEl, {
+            targetEl: targetEl,
+            triggerEl: triggerEl
+        })
+    })
+}
+
+if (document.readyState !== 'loading') {
+	// DOMContentLoaded event were already fired. Perform explicit initialization now
+	initDial()
+} else {
+	// DOMContentLoaded event not yet fired, attach initialization process to it
+	document.addEventListener('DOMContentLoaded', initDial)
+}
+
+export default Dial

9a7c3e1618afee400e25a2bf6ac96e4e34d0d5fa
Zoltán Szőgyényi <zoltan.szogyenyi@gmail.com>
2022-12-10 12:13:05 +0200

    Add new dropdown offset options and examples. Fixes https://github.com/themesberg/flowbite/issues/237.

--- src/components/dropdown/index.js
+++ src/components/dropdown/index.js
@@ -3,25 +3,27 @@ import { createPopper } from '@popperjs/core';
 const Default = {
     placement: 'bottom',
     triggerType: 'click',
+    offsetSkidding: 0,
+    offsetDistance: 10,
     onShow: () => { },
     onHide: () => { }
-}
+};
 
 class Dropdown {
     constructor(targetElement = null, triggerElement = null, options = {}) {
-        this._targetEl = targetElement
-        this._triggerEl = triggerElement
-        this._options = { ...Default, ...options }
-        this._popperInstance = this._createPopperInstace()
-        this._visible = false
-        this._init()
+        this._targetEl = targetElement;
+        this._triggerEl = triggerElement;
+        this._options = { ...Default, ...options };
+        this._popperInstance = this._createPopperInstace();
+        this._visible = false;
+        this._init();
     }
 
     _init() {
         if (this._triggerEl) {
             this._triggerEl.addEventListener('click', () => {
-                this.toggle()
-            })
+                this.toggle();
+            });
         }
     }
 
@@ -32,7 +34,7 @@ class Dropdown {
                 {
                     name: 'offset',
                     options: {
-                        offset: [0, 10],
+                        offset: [this._options.offsetSkidding, this._options.offsetDistance],
                     },
                 },
             ],
@@ -40,28 +42,28 @@ class Dropdown {
     }
 
     _handleClickOutside(ev, targetEl) {
-        const clickedEl = ev.target
+        const clickedEl = ev.target;
         if (clickedEl !== targetEl && !targetEl.contains(clickedEl) && !this._triggerEl.contains(clickedEl) && this._visible) {
-            this.hide()
+            this.hide();
         }
-        document.body.removeEventListener('click', this._handleClickOutside, true)
+        document.body.removeEventListener('click', this._handleClickOutside, true);
     }
 
     toggle() {
         if (this._visible) {
-            this.hide()
-            document.body.removeEventListener('click', this._handleClickOutside, true)
+            this.hide();
+            document.body.removeEventListener('click', this._handleClickOutside, true);
         } else {
-            this.show()
+            this.show();
         }
     }
 
     show() {
-        this._targetEl.classList.remove('hidden')
-        this._targetEl.classList.add('block')
+        this._targetEl.classList.remove('hidden');
+        this._targetEl.classList.add('block');
 
         // Enable the event listeners
-        this._popperInstance.setOptions(options => ({
+        this._popperInstance.setOptions((options) => ({
             ...options,
             modifiers: [
                 ...options.modifiers,
@@ -69,47 +71,51 @@ class Dropdown {
             ],
         }));
 
-        document.body.addEventListener('click', (ev) => { this._handleClickOutside(ev, this._targetEl) }, true)
+        document.body.addEventListener('click', (ev) => { this._handleClickOutside(ev, this._targetEl); }, true);
 
         // Update its position
-        this._popperInstance.update()
-        this._visible = true
+        this._popperInstance.update();
+        this._visible = true;
 
         // callback function
-        this._options.onShow(this)
+        this._options.onShow(this);
     }
 
     hide() {
-        this._targetEl.classList.remove('block')
-        this._targetEl.classList.add('hidden')
+        this._targetEl.classList.remove('block');
+        this._targetEl.classList.add('hidden');
 
         // Disable the event listeners
-        this._popperInstance.setOptions(options => ({
+        this._popperInstance.setOptions((options) => ({
             ...options,
             modifiers: [
                 ...options.modifiers,
                 { name: 'eventListeners', enabled: false },
             ],
-        }))
+        }));
 
-        this._visible = false
+        this._visible = false;
 
         // callback function
-        this._options.onHide(this)
+        this._options.onHide(this);
     }
 }
 
 window.Dropdown = Dropdown;
 
 export function initDropdowns() {
-    document.querySelectorAll('[data-dropdown-toggle]').forEach(triggerEl => {
-        const targetEl = document.getElementById(triggerEl.getAttribute('data-dropdown-toggle'))
-        const placement = triggerEl.getAttribute('data-dropdown-placement')
+    document.querySelectorAll('[data-dropdown-toggle]').forEach((triggerEl) => {
+        const targetEl = document.getElementById(triggerEl.getAttribute('data-dropdown-toggle'));
+        const placement = triggerEl.getAttribute('data-dropdown-placement');
+        const offsetSkidding = triggerEl.getAttribute('data-dropdown-offset-skidding');
+        const offsetDistance = triggerEl.getAttribute('data-dropdown-offset-distance');
 
         new Dropdown(targetEl, triggerEl, {
-            placement: placement ? placement : Default.placement
-        })
-    })
+            placement: placement ? placement : Default.placement,
+            offsetSkidding: offsetSkidding ? parseInt(offsetSkidding) : Default.offsetSkidding,
+            offsetDistance: offsetDistance ? parseInt(offsetDistance) : Default.offsetDistance,
+        });
+    });
 }
 
-export default Dropdown
+export default Dropdown;

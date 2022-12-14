7595e1db4147a36725bca0112ef412264a0e5237
Zoltán Szőgyényi <zoltan.szogyenyi@gmail.com>
2022-12-14 14:01:33 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
     root: true,
     parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint', 'jsx-a11y'],
+    plugins: ['@typescript-eslint'],
     extends: [
         'eslint:recommended',
         'plugin:@typescript-eslint/eslint-recommended',

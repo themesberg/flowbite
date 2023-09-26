1c98d41c21ba2ae09106f13f0fd8c34730e24723
Zoltán Szőgyényi <zoltan.szogyenyi@gmail.com>
2023-09-26 13:04:26 +0300

    feat(autocomplete): update files to `.ts`

--- content/forms/autocomplete.md
+++ content/forms/autocomplete.md
@@ -1,3 +1,4 @@
+
 ---
 layout: home
 title: Tailwind CSS Autocomplete - Flowbite
@@ -18,7 +19,7 @@ The select input component can be used to gather information from users based on
 Get started with the default example of a select input component to get a single option selection.
 
 {{< example class="dark:bg-gray-800" github="forms/select.md" show_dark=true >}}
-<!-- <div class="relative w-80">
+<div class="relative w-80">
     <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
         <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
     </div>
@@ -40,7 +41,7 @@ Get started with the default example of a select input component to get a single
             </li>
         </ul>
     </div>
-</div> -->
+</div>
 {{< /example >}}
 
 ## JavaScript behaviour
--- src/components/autocomplete/index.ts
+++ src/components/autocomplete/index.ts
@@ -1,86 +1,91 @@
-const autocomplete = document.getElementById('autocomplete')
-const data = document.getElementById('autocomplete-data')
+/* eslint-disable @typescript-eslint/no-empty-function */
+// import type { AccordionItem, AccordionOptions } from './types';
+// import { AccordionInterface } from './interface';
+
+const autocomplete = document.getElementById('autocomplete');
+const data = document.getElementById('autocomplete-data');
 
 const Default = {
     // optional
     label: {
         text: 'Recent',
-        classes: 'px-5 mb-3 font-semibold text-gray-900'
+        classes: 'px-5 mb-3 font-semibold text-gray-900',
     },
-    containerClasses: 'absolute bg-white border mt-2 border-gray-200 rounded-lg z-[60] text-gray-500 text-sm py-3 w-full hidden',
+    containerClasses:
+        'absolute bg-white border mt-2 border-gray-200 rounded-lg z-[60] text-gray-500 text-sm py-3 w-full hidden',
     menuClasses: 'font-medium text-gray-500',
-    menuItemClasses: 'flex items-center py-2 px-5 cursor-pointer hover:bg-gray-100',
-    onHide: () => { }
-}
+    menuItemClasses:
+        'flex items-center py-2 px-5 cursor-pointer hover:bg-gray-100',
+    onHide: () => {},
+};
 
 class Autocomplete {
     constructor(inputEl = null, source = [], options = {}) {
-        this._inputEl = inputEl
-        this._source = source
-        this._containerEl = null
-        this._options = { ...Default, ...options }
-        this._init()
+        this._inputEl = inputEl;
+        this._source = source;
+        this._containerEl = null;
+        this._options = { ...Default, ...options };
+        this._init();
     }
 
     _init() {
-        this._initMenu()
+        this._initMenu();
 
         this._inputEl.addEventListener('focus', () => {
-            this.showMenu()
-        })
+            this.showMenu();
+        });
 
         this._inputEl.addEventListener('blur', () => {
             // this.hideData()
-        })
+        });
     }
 
     _initMenu() {
-        const containerEl = document.createElement('div')
-        containerEl.classList.add(...this._options.containerClasses.split(" "))
+        const containerEl = document.createElement('div');
+        containerEl.classList.add(...this._options.containerClasses.split(' '));
 
-        const labelEl = document.createElement('div')
-        labelEl.innerHTML = this._options.label.text
-        labelEl.classList.add(...this._options.label.classes.split(" "))
+        const labelEl = document.createElement('div');
+        labelEl.innerHTML = this._options.label.text;
+        labelEl.classList.add(...this._options.label.classes.split(' '));
 
-        const menuEl = document.createElement('ul')
-        menuEl.setAttribute('role', 'listbox')
-        menuEl.classList.add(...this._options.menuClasses.split(" "))
+        const menuEl = document.createElement('ul');
+        menuEl.setAttribute('role', 'listbox');
+        menuEl.classList.add(...this._options.menuClasses.split(' '));
 
-        containerEl.appendChild(labelEl)
+        containerEl.appendChild(labelEl);
 
-        console.log(this._source)
+        console.log(this._source);
 
         this._source.map((s, i) => {
-            console.log(s)
-            console.log(i)
-            const itemEl = this._createItemEl(s, i)
-            menuEl.appendChild(itemEl)
-        })
+            console.log(s);
+            console.log(i);
+            const itemEl = this._createItemEl(s, i);
+            menuEl.appendChild(itemEl);
+        });
 
-        labelEl.after(menuEl)
-        this._inputEl.after(containerEl)
+        labelEl.after(menuEl);
+        this._inputEl.after(containerEl);
 
         // set container el to get reference
-        this._containerEl = containerEl
+        this._containerEl = containerEl;
 
-        console.log(containerEl)
+        console.log(containerEl);
     }
 
     _createItemEl(source, index) {
-        const itemEl = document.createElement('li')
-        itemEl.setAttribute('role', 'presentation')
-        itemEl.setAttribute('tabindex', -1)
-        itemEl.setAttribute('aria-posinset', index)
-        itemEl.setAttribute('aria-selected', 'false')
-        itemEl.classList.add(...this._options.menuItemClasses.split(" "))
-        itemEl.innerHTML = `${source.icon} <span>${source.value}</span>`
+        const itemEl = document.createElement('li');
+        itemEl.setAttribute('role', 'presentation');
+        itemEl.setAttribute('tabindex', -1);
+        itemEl.setAttribute('aria-posinset', index);
+        itemEl.setAttribute('aria-selected', 'false');
+        itemEl.classList.add(...this._options.menuItemClasses.split(' '));
+        itemEl.innerHTML = `${source.icon} <span>${source.value}</span>`;
 
-        return itemEl
+        return itemEl;
     }
 
     showMenu() {
-        this._containerEl.classList.remove('hidden')
-
+        this._containerEl.classList.remove('hidden');
 
         // Object.values(this._dataEl.options).map(o => {
         //     o.classList.add('block')
@@ -90,20 +95,21 @@ class Autocomplete {
     hideData() {
         // this._dataEl.classList.remove('block')
     }
-
-} 
+}
 
 window.Autocomplete = Autocomplete;
 
 document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('[data-autocomplete]').forEach(inputEl => {
-        const menuEl = document.getElementById(inputEl.getAttribute('data-autocomplete'))
+    document.querySelectorAll('[data-autocomplete]').forEach((inputEl) => {
+        const menuEl = document.getElementById(
+            inputEl.getAttribute('data-autocomplete')
+        );
 
-        new Autocomplete(inputEl, menuEl, {})
-    })
-})
+        new Autocomplete(inputEl, menuEl, {});
+    });
+});
 
-const autocompleteEl = document.getElementById('autocomplete-js')
+const autocompleteEl = document.getElementById('autocomplete-js');
 
 new Autocomplete(autocompleteEl, [
     {
@@ -130,9 +136,9 @@ new Autocomplete(autocompleteEl, [
         icon: `<svg class="mr-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>`,
         value: 'France',
     },
-])
+]);
 
-export default Autocomplete
+export default Autocomplete;
 
 /*
 autocomplete.onfocus = function () {

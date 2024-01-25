504d091fd8ef1d9b309e5e4d490471e6d06e19b4
Zoltán Szőgyényi <zoltan.szogyenyi@gmail.com>
2024-01-25 13:06:45 +0200

    chore(drawer): linting for JS file

--- src/components/drawer/index.ts
+++ src/components/drawer/index.ts
@@ -217,7 +217,10 @@ class Drawer implements DrawerInterface {
     }
 
     _destroyBackdropEl() {
-        if (this._visible && document.querySelector('[drawer-backdrop]') !== null) {
+        if (
+            this._visible &&
+            document.querySelector('[drawer-backdrop]') !== null
+        ) {
             document.querySelector('[drawer-backdrop]').remove();
         }
     }

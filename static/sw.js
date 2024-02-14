fa188127019b48e4a89952f61b5c29c1bc7573fe
Zoltán Szőgyényi <zoltan.szogyenyi@gmail.com>
2024-02-14 13:19:58 +0200

    chore(sw): linting for the sw.js page

--- static/sw.js
+++ static/sw.js
@@ -3,25 +3,28 @@
 // ++++++++++++++++++++++++++++++++++++++++++
 
 (function () {
-  'use strict'
+    'use strict';
 
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function () {
-      navigator.serviceWorker.getRegistrations().then(function (registrations) {
-        for (var registration of registrations) {
-          registration.unregister()
-            .then(function () {
-              return self.clients.matchAll()
-            })
-            .then(function (clients) {
-              clients.forEach(function (client) {
-                if (client.url && 'navigate' in client) {
-                  client.navigate(client.url)
-                }
-              })
-            })
-        }
-      })
-    })
-  }
-})()
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', function () {
+            navigator.serviceWorker
+                .getRegistrations()
+                .then(function (registrations) {
+                    for (var registration of registrations) {
+                        registration
+                            .unregister()
+                            .then(function () {
+                                return self.clients.matchAll();
+                            })
+                            .then(function (clients) {
+                                clients.forEach(function (client) {
+                                    if (client.url && 'navigate' in client) {
+                                        client.navigate(client.url);
+                                    }
+                                });
+                            });
+                    }
+                });
+        });
+    }
+})();

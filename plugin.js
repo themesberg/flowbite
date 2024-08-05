b30fd485550c24946dbc59967ce456e0ca4cc4ec
Zoltán Szőgyényi <zoltan.szogyenyi@gmail.com>
2024-08-05 15:02:51 +0300

    fix(plugin): remove import

--- plugin.js
+++ plugin.js
@@ -3,7 +3,6 @@ const svgToDataUri = require('mini-svg-data-uri');
 const plugin = require('tailwindcss/plugin');
 const defaultTheme = require('tailwindcss/defaultTheme');
 const colors = require('tailwindcss/colors');
-const { transform } = require('typescript');
 const [baseFontSize, { lineHeight: baseLineHeight }] =
     defaultTheme.fontSize.base;
 const { spacing, borderWidth, borderRadius, boxShadow } = defaultTheme;
